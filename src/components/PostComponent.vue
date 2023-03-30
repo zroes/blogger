f69e6bea856609154d5bc0e76d90d1a50c348fc9
josh <joshjj3@gmail.com>
2023-03-30 16:37:03 -0600

    push this now

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,14 +1,14 @@
 <template>
-  <router-link :to="{ name: 'Post', params: { postId: post?.postId } }">
+    <router-link :to="{ name: 'Post', params: { postId: post?.id } }">
 
-    <div @click.stop="goToProfile(post?.creator.id)">
-      <img class="p-2 profile-img" :src="post?.creator.picture" :alt="post?.creator.name" :title="post?.creator.name">
-      <span class="p-2">{{ post?.creator.name }}</span>
-    </div>
-    <div>
-      <p>{{ post?.title }}</p>
-    </div>
-  </router-link>
+        <div @click.stop="goToProfile(post?.creator.id)">
+            <img class="p-2 profile-img" :src="post?.creator.picture" :alt="post?.creator.name" :title="post?.creator.name">
+            <span class="p-2">{{ post?.creator.name }}</span>
+        </div>
+        <div>
+            <p>{{ post?.title }}</p>
+        </div>
+    </router-link>
 </template>
 
 
@@ -17,29 +17,29 @@ import { useRouter } from 'vue-router'
 import { Post } from '../models/Post.js'
 
 export default {
-  props: {
-    post: {
-      type: Post,
-      required: true
+    props: {
+        post: {
+            type: Post,
+            required: true
+        }
+    },
+    setup(props) {
+        const router = useRouter()
+        return {
+            goToProfile() {
+                router.push({ name: 'Profile', params: { profileId: props.post.creatorId } })
+            }
+        }
     }
-  },
-  setup(props) {
-    const router = useRouter()
-    return {
-      goToProfile() {
-        router.push({ name: 'Profile', params: { profileId: props.post.creatorId } })
-      }
-    }
-  }
 }
 </script>
 
 
 <style lang="scss" scoped>
 .profile-img {
-  height: 10vh;
-  width: 10vh;
-  border-radius: 50%;
+    height: 10vh;
+    width: 10vh;
+    border-radius: 50%;
 
 }
 </style>
