eaa8ab0051d5eb1b95eaecfe06fb30d4c9a82693
josh <joshjj3@gmail.com>
2023-03-30 23:33:14 -0600

    started on the form but cant finish no accounts

---
+++ src/components/CreatePostInsert.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="floating-form mb-3">
+        <input v-model="editable.body" class="form-control" id="floatingInput" placeholder="type out your feelings bro"
+            type="text">
+        <!-- TODO :disabled="account.id ? 'false' : 'true'" ask about this -->
+        <label for="floatingInput"></label>
+    </div>
+</template>
+
+
+<script>
+import { ref, computed } from 'vue';
+import { AppState } from '../AppState.js';
+
+export default {
+    setup() {
+        const editable = ref({})
+        return {
+            account: computed(() => AppState.account),
+            editable
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container-fluid">
     <section class="row justify-content-center">
+      <div class="col-10">
+        <CreatePostInsert />
+
+      </div>
+    </section>
+    <section class="row justify-content-center">
       <div class="col-10 custom-border rounded elevation my-2" v-for="p in posts" :key="p.id">
         <PostComponent :post="p" />
       </div>
@@ -14,6 +20,7 @@ import { logger } from '../utils/Logger.js'
 import { postsService } from '../services/PostsService.js'
 import { AppState } from '../AppState.js'
 import PostComponent from '../components/PostComponent.vue'
+import CreatePostInsert from '../components/CreatePostInsert.vue'
 export default {
   setup() {
     async function getPosts() {
@@ -25,14 +32,19 @@ export default {
       }
     }
 
+    async function getAccount() {
+      logger.log(AppState.account, 'getting account')
+    }
+
     onMounted(() => {
-      getPosts()
+      getPosts(),
+        getAccount()
     })
     return {
       posts: computed(() => AppState.posts)
     }
   },
-  components: { PostComponent }
+  components: { PostComponent, CreatePostInsert }
 }
 </script>
 
